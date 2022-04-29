516081bf9fc5fd87a78d70f09198d7d058a792c6
cdpaiva <cdpaivaa@gmail.com>
2022-04-29 10:44:14 -0400

    Add new content to the sidebar

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,16 +4,31 @@ module.exports = {
     description: 'A personal blog to document public learning Vue.js and frontend development',
     themeConfig: {
         nav: [
-            {text: 'Home', link: '/'},
-            {text: 'Projects', link: '/projects/'},
-            {text: 'Notes', link: '/notes/'},
-            {text: 'About', link: '/about/'}
+            { text: 'Home', link: '/' },
+            { text: 'Projects', link: '/projects/' },
+            { text: 'Notes', link: '/notes/' },
+            { text: 'About', link: '/about/' }
         ],
         sidebar: [
-            {text: 'Vue Weather', link: '/projects/vue-weather',
+            {
+                text: 'Projects',
+                children: [
+                    {
+                        text: 'Vue Weather',
+                        link: '/projects/vue-weather',
+                    },
+                    {
+                        text: 'Project Manager',
+                        link: '/projects/vue-project-manager'
+                    },
+                    {
+                        text: 'Vue Puzzle Slider',
+                        link: '/projects/vue-puzzle-slider'
+                    },
+                ]
             },
-            {text: 'Project Manager', link: '/projects/vue-project-manager'},
-            {text: 'Vue Puzzle Slider', link: '/projects/vue-puzzle-slider'},
+
+            { text: 'Notes', link: '/notes/' }
 
         ]
     }
